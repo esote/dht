f1d15dde06fc2d4ddeb6d9b311fe29d589ae742e
esote <esote.net@gmail.com>
2020-08-19 11:50:05 -0500

    use struct, fix dynamic x & buf sizes, fix c1 & c2 verif

--- skad.go
+++ skad.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"crypto/ed25519"
-	"crypto/rand"
+	ed "crypto/ed25519"
+	cryptorand "crypto/rand"
 	"crypto/sha512"
 	"encoding/hex"
 	"errors"
@@ -10,147 +10,145 @@ import (
 	"hash"
 	"log"
 	"math/bits"
-	"time"
 
 	"golang.org/x/crypto/sha3"
 )
 
-func main() {
-	// Static
-	const c1 = 8 * 1
-	start := time.Now()
-	publ, priv, err := Static(c1, sha512.New())
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("publ:", hex.EncodeToString(publ))
-	fmt.Println("priv:", hex.EncodeToString(priv))
-	fmt.Println("static took:", time.Now().Sub(start))
-
-	// Dynamic from publ
-	const c2 = c1
-	start = time.Now()
-	x, err := Dynamic(c2, sha512.New(), publ)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("x:", hex.EncodeToString(x))
-	fmt.Println("dynamic took:", time.Now().Sub(start))
-
-	// TODO: validation func to check (publ, x) meets c1, c2 requirements
-	fmt.Println("valid:", Verify(c1, c2, sha512.New(), sha512.New(), publ, x))
+type CryptoPuzzle struct {
+	c1, c2 int
+	h1, h2 hash.Hash
 }
 
-// TODO: define struct to keep c1, c2, h1, h2 for gen and verif
-// TODO: best way to chooose c1, c2? increment them until a certain time thresh?
-// Use sha512 for static and sha3-512 for dynamic for diversify hashes used.
-func KeypairFromPuzzles() (ed25519.PublicKey, ed25519.PrivateKey, []byte, error) {
-	publ, priv, err := Static(8*2, sha512.New())
+func New(c1, c2 int, h1, h2 hash.Hash) (*CryptoPuzzle, error) {
+	if c1 == 0 || c1 > 8*h1.Size() {
+		return nil, errors.New("c1 invalid")
+	}
+	if c2 == 0 || c2 > 8*h2.Size() {
+		return nil, errors.New("c2 invalid")
+	}
+	return &CryptoPuzzle{c1, c2, h1, h2}, nil
+}
+
+func (c *CryptoPuzzle) Generate() (ed.PublicKey, ed.PrivateKey, []byte, error) {
+	publ, priv, err := c.static()
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	x, err := Dynamic(8*2, sha3.New512(), publ)
+	x, err := c.dynamic(publ)
 	if err != nil {
 		return nil, nil, nil, err
 	}
 	return publ, priv, x, nil
 }
 
-// Verify public key chosen from KeypairFromPuzzles
-func VerifyFromPuzzles(publ ed25519.PublicKey, x []byte) bool {
-	return Verify(8*2, 8*2, sha512.New(), sha3.New512(), publ, x)
-}
-
-func Verify(c1, c2 int, h1, h2 hash.Hash, publ ed25519.PublicKey, x []byte) bool {
-	h1.Reset()
-	if _, err := h1.Write(publ); err != nil {
+func (c *CryptoPuzzle) Verify(publ ed.PublicKey, x []byte) bool {
+	c.h1.Reset()
+	if _, err := c.h1.Write(publ); err != nil {
 		return false
 	}
-	r1 := h1.Sum(nil)
-	h1.Reset()
-	if _, err := h1.Write(r1); err != nil {
+	r1 := c.h1.Sum(nil)
+	c.h1.Reset()
+	if _, err := c.h1.Write(r1); err != nil {
 		return false
 	}
-	r1 = h1.Sum(r1[:0])
-	if LeadingZeros(r1) != c1 {
+	r1 = c.h1.Sum(r1[:0])
+	if LeadingZeros(r1) != c.c1 {
 		return false
 	}
 
-	h2.Reset()
-	if _, err := h2.Write(publ); err != nil {
+	c.h2.Reset()
+	if _, err := c.h2.Write(publ); err != nil {
 		return false
 	}
-	r2 := h2.Sum(nil)
+	r2 := c.h2.Sum(nil)
 	if len(x) != len(r2) {
 		return false
 	}
 	Xor(r2, r2, x)
-	h2.Reset()
-	if _, err := h2.Write(r2); err != nil {
+	c.h2.Reset()
+	if _, err := c.h2.Write(r2); err != nil {
 		return false
 	}
-	r2 = h2.Sum(r2[:0])
-	return LeadingZeros(r2) == c2
+	r2 = c.h2.Sum(r2[:0])
+	return LeadingZeros(r2) == c.c2
 }
 
-func Static(c1 int, h hash.Hash) (ed25519.PublicKey, ed25519.PrivateKey, error) {
-	if c1 == 0 || c1 > 8*ed25519.PublicKeySize {
-		return nil, nil, errors.New("c1 invalid")
-	}
-	// Allocate p early to reuse it.
-	p := make([]byte, h.Size())
+func (c *CryptoPuzzle) static() (ed.PublicKey, ed.PrivateKey, error) {
+	p := make([]byte, c.h1.Size())
 	for {
-		publ, priv, err := ed25519.GenerateKey(rand.Reader)
+		publ, priv, err := ed.GenerateKey(nil)
 		if err != nil {
 			return nil, nil, err
 		}
-		h.Reset()
-		if _, err = h.Write(publ); err != nil {
+		c.h1.Reset()
+		if _, err = c.h1.Write(publ); err != nil {
 			return nil, nil, err
 		}
-		p = h.Sum(p[:0])
-		h.Reset()
-		if _, err = h.Write(p); err != nil {
+		p = c.h1.Sum(p[:0])
+		c.h1.Reset()
+		if _, err = c.h1.Write(p); err != nil {
 			return nil, nil, err
 		}
-		p = h.Sum(p[:0])
-		if LeadingZeros(p) == c1 {
+		p = c.h1.Sum(p[:0])
+		if LeadingZeros(p) == c.c1 { // TODO: >= c.1 ?
 			return publ, priv, nil
 		}
 	}
 }
 
-func Dynamic(c2 int, h hash.Hash, publ ed25519.PublicKey) ([]byte, error) {
-	if c2 == 0 || c2 > 8*ed25519.PublicKeySize {
-		return nil, errors.New("c2 invalid")
-	}
-	x := make([]byte, h.Size())
-	buf := make([]byte, h.Size())
-	var err error
-	h.Reset()
-	if _, err = h.Write(publ); err != nil {
+func (c *CryptoPuzzle) dynamic(publ ed.PublicKey) ([]byte, error) {
+	x := make([]byte, c.h2.Size())
+	buf := make([]byte, c.h2.Size())
+	c.h2.Reset()
+	if _, err := c.h2.Write(publ); err != nil {
 		return nil, err
 	}
-	prex := h.Sum(nil)
-	// Allocate p early to reuse it.
-	p := make([]byte, h.Size())
+	prex := c.h2.Sum(nil)
+	p := make([]byte, c.h2.Size())
 	for {
-		if _, err = rand.Read(x); err != nil {
+		if _, err := cryptorand.Read(x); err != nil {
 			return nil, err
 		}
 		Xor(buf, prex, x)
-		h.Reset()
-		if _, err = h.Write(buf); err != nil {
+		c.h2.Reset()
+		if _, err := c.h2.Write(buf); err != nil {
 			return nil, err
 		}
-		p := h.Sum(p[:0])
-		if LeadingZeros(p) == c2 {
+		p := c.h2.Sum(p[:0])
+		if LeadingZeros(p) == c.c2 { // TODO: >= c.2 ?
 			return x, nil
 		}
 	}
 }
 
-// TODO: assembly impl?
+func main() {
+	// TODO: best way to chooose c1, c2? increment them until a certain time
+	// threshhold?
+	const (
+		c1 = 8*3 + 0
+		c2 = 8*3 + 0
+	)
+	var (
+		h1 = sha512.New()
+		h2 = sha3.New512()
+	)
+	c, err := New(c1, c2, h1, h2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	publ, priv, x, err := c.Generate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("publ:", hex.EncodeToString(publ))
+	fmt.Println("priv:", hex.EncodeToString(priv))
+	fmt.Println("x:", hex.EncodeToString(x))
+	fmt.Println("valid:", c.Verify(publ, x))
+}
+
+// Count leading zero bits.
+// TODO: assembly impl using SIMD instructions. If len(data) == 32, VPLZCNTQ
+// from AVX-512 could check the entire thing in one instruction.
 func LeadingZeros(data []byte) int {
 	for i := 0; i < len(data); i++ {
 		if data[i] != 0 {
@@ -161,6 +159,7 @@ func LeadingZeros(data []byte) int {
 }
 
 // Xor assumes len(a) == len(b) == len(dst)
+// TODO: assembly impl using SIMD instructions.
 func Xor(dst, a, b []byte) {
 	for i := 0; i < len(dst); i++ {
 		dst[i] = a[i] ^ b[i]
--- skad_test.go
+++ skad_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha512"
+	"hash"
 	"testing"
 
 	"golang.org/x/crypto/sha3"
@@ -13,54 +14,62 @@ const (
 )
 
 func TestCryptoPuzzle(t *testing.T) {
-	publ, _, x, err := KeypairFromPuzzles()
+	c, err := New(c1, c2, sha512.New(), sha512.New())
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !VerifyFromPuzzles(publ, x) {
+	publ, _, x, err := c.Generate()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !c.Verify(publ, x) {
 		t.Fatal("publ invalid")
 	}
 }
 
-func BenchmarkStatic_SHA512(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		if _, _, err := Static(c1, sha512.New()); err != nil {
-			b.Fatal(err)
-		}
-	}
+func BenchmarkStatic(b *testing.B) {
+	b.Run("sha512", func(b *testing.B) {
+		benchmarkStatic(b, sha512.New())
+	})
+	b.Run("sha3-512", func(b *testing.B) {
+		benchmarkStatic(b, sha3.New512())
+	})
 }
 
-func BenchmarkStatic_SHA3_512(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		if _, _, err := Static(c1, sha3.New512()); err != nil {
-			b.Fatal(err)
-		}
-	}
+func BenchmarkDynamic(b *testing.B) {
+	b.Run("sha512", func(b *testing.B) {
+		benchmarkDynamic(b, sha512.New())
+	})
+	b.Run("sha3-512", func(b *testing.B) {
+		benchmarkDynamic(b, sha3.New512())
+	})
 }
 
-func BenchmarkDynamic_SHA512(b *testing.B) {
-	h := sha512.New()
-	publ, _, err := Static(c1, h)
+func benchmarkStatic(b *testing.B, h hash.Hash) {
+	c, err := New(c1, c2, h, h)
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if _, err = Dynamic(c2, h, publ); err != nil {
+		if _, _, err = c.static(); err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
-func BenchmarkDynamic_SHA3_512(b *testing.B) {
-	h := sha3.New512()
-	publ, _, err := Static(c1, h)
+func benchmarkDynamic(b *testing.B, h hash.Hash) {
+	c, err := New(c1, c2, h, h)
+	if err != nil {
+		b.Fatal(err)
+	}
+	publ, _, err := c.static()
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if _, err = Dynamic(c2, h, publ); err != nil {
+		if _, err = c.dynamic(publ); err != nil {
 			b.Fatal(err)
 		}
 	}
