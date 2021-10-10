6eeb69a55e4cbbd3aab0da8938261129cdbe803d
esote <esote.net@gmail.com>
2021-10-10 12:18:32 -0500

    always use compat funcs for POSIX+XSI compliance, fix imsg.h build

--- c/src/compat/compat.h
+++ c/src/compat/compat.h
@@ -2,32 +2,11 @@
 
 #include <limits.h> /* for IOV_MAX with _XOPEN_SOURCE=700 */
 
-#ifndef HAVE_EXPLICIT_BZERO
-void explicit_bzero(void *, size_t);
-#endif
-
-#ifndef HAVE_FREEZERO
-void freezero(void *, size_t);
-#endif
-
-#ifndef HAVE_GETDTABLECOUNT
-int getdtablecount(void);
-#endif
-
-#ifndef HAVE_GETDTABLESIZE
-int getdtablesize(void);
-#endif
-
-#ifndef HAVE_GETPAGESIZE
-int getpagesize(void);
-#endif
-
-#ifdef HAVE_QUEUE
-#include <sys/queue.h>
-#else
 #include "queue.h"
-#endif
 
-#ifndef HAVE_RECALLOCARRAY
+void explicit_bzero(void *, size_t);
+void freezero(void *, size_t);
+int getdtablecount(void);
+int getdtablesize(void);
+int getpagesize(void);
 void *recallocarray(void *, size_t, size_t, size_t);
-#endif
